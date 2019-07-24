634078baca1e25f6b6cfbdd3ca1673fd67482605
Seschwan <seschwan@Seschwans-MacBook-Pro.local>
2019-07-23 21:29:24 -0600

    Project Setup

---
+++ iOS Albums/iOS Albums/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Albums
+//
+//  Created by Seschwan on 7/23/19.
+//  Copyright © 2019 Seschwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
